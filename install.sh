27ce5c97791b2be6b63952adf96e025bf9648282
MEHDI <MehdiSalmanzadeh@outlook.com>
2024-03-16 02:27:00 +0330

    Chore: Change color variable from PURPLE to BLUE in install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PURPLE='\033[0;35m'
-PURPLE='\033[1;34m'
+BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 # Function to clear the terminal screen
